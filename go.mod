c4663853d9ffbf54a8bc63805b21f3fea2ccfe65
Nicolas Bock <nicolasbock@gmail.com>
2023-08-15 13:42:19 -0600

    Add parser
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nicolasbock/yet-another-sort/v2
 
 go 1.20
+
+require github.com/akamensky/argparse v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"github.com/akamensky/argparse"
+	"os"
+)
+
 func main() {
-	
+	var parser *argparse.Parser = argparse.NewParser("yet-another-sort",
+		`Yet-another-sort is yet another command line utility that sorts lines in a file.`)
+	var err error = parser.Parse(os.Args)
+	if err != nil {
+		fmt.Println(parser.Usage(err))
+	}
 }
