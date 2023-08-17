0048f4154cb64fe51378c578ab4bd6e9d657a5a2
Nicolas Bock <nicolasbock@gmail.com>
2023-08-17 07:21:18 -0600

    Add bash completion
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

---
+++ scripts/yet-another-sort-bash-completion.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+_complete_yet_another_sort () {
+  local cur prev
+  local known_options=(
+    --debug
+    --field-separator
+    --key
+    --multiline
+    -h --help
+  )
+
+  _init_completion || return
+
+  case "$prev" in
+    --folder|--destination)
+      _filedir
+      return
+      ;;
+    --print-database-format)
+      readarray -t COMPREPLY < <(compgen -W 'CSV JSON YAML' -- "${cur}")
+      return
+      ;;
+    --destination-option)
+      readarray -t COMPREPLY < <(compgen -W 'panic delete append' -- "${cur}")
+  esac
+
+  if [[ "$cur" == -* ]]; then
+    readarray -t COMPREPLY < <(compgen -W "${known_options[*]}" -- "${cur}" )
+    return
+  fi
+
+  _filedir
+}
+
+complete -F _complete_yet_another_sort pick-files
