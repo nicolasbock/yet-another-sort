4f04b64c1ac823a24c3f5f85f470c11b696c7578
Nicolas Bock <nicolasbock@gmail.com>
2023-08-18 15:32:47 -0600

    Add tests for sorting
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

---
+++ sort_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var input ContentType = ContentType{
+	ContentLineType{Lines: []string{"2. Second line"}, Fields: []string{"2.", "Second", "line"}},
+	ContentLineType{Lines: []string{"1. First line"}, Fields: []string{"1.", "First", "line"}},
+	ContentLineType{Lines: []string{"4. Fourth line"}, Fields: []string{"4.", "Fourth", "line"}},
+	ContentLineType{Lines: []string{"5. Fifth line"}, Fields: []string{"5.", "Fifth", "line"}},
+	ContentLineType{Lines: []string{"3. Third line"}, Fields: []string{"3.", "Third", "line"}},
+}
+
+func TestSort1(t *testing.T) {
+	var expected ContentType = ContentType{
+		ContentLineType{Lines: []string{"1. First line"}, Fields: []string{"1.", "First", "line"}},
+		ContentLineType{Lines: []string{"2. Second line"}, Fields: []string{"2.", "Second", "line"}},
+		ContentLineType{Lines: []string{"3. Third line"}, Fields: []string{"3.", "Third", "line"}},
+		ContentLineType{Lines: []string{"4. Fourth line"}, Fields: []string{"4.", "Fourth", "line"}},
+		ContentLineType{Lines: []string{"5. Fifth line"}, Fields: []string{"5.", "Fifth", "line"}},
+		}
+	var got ContentType = SortContents(input, 1)
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("got %s, expected %s", got, expected)
+	}
+}
+
+func TestSort2(t *testing.T) {
+	var expected ContentType = ContentType{
+		ContentLineType{Lines: []string{"5. Fifth line"}, Fields: []string{"5.", "Fifth", "line"}},
+		ContentLineType{Lines: []string{"1. First line"}, Fields: []string{"1.", "First", "line"}},
+		ContentLineType{Lines: []string{"4. Fourth line"}, Fields: []string{"4.", "Fourth", "line"}},
+		ContentLineType{Lines: []string{"2. Second line"}, Fields: []string{"2.", "Second", "line"}},
+		ContentLineType{Lines: []string{"3. Third line"}, Fields: []string{"3.", "Third", "line"}},
+		}
+	var got ContentType = SortContents(input, 2)
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("got %s, expected %s", got, expected)
+	}
+}
