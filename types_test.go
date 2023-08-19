b541a40ab887fb6371954f9ae56e5d28ed701824
Nicolas Bock <nicolasbock@gmail.com>
2023-08-19 17:07:25 -0600

    Remove trailing newline
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- types_test.go
+++ types_test.go
@@ -18,7 +18,7 @@ func TestKeyTypeString(t *testing.T) {
 
 func TestContentLineType(t *testing.T) {
 	var c ContentLineType = ContentLineType{Lines: []string{"first line"}, Fields: []string{"first", "line"}, CompareLine: "first"}
-	var expected string = "multiline\n  line: 'first line'\n  fields: first, line\n  compare: 'first'\n"
+	var expected string = "multiline\n  line: 'first line'\n  fields: first, line\n  compare: 'first'"
 	var got string = c.String()
 	if strings.Compare(got, expected) != 0 {
 		t.Errorf("Expected\n%s\ngot\n%s", expected, got)
