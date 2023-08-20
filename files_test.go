5aa9ed024ab3c02576c6ffa5f52d8a86f1c99ac3
Nicolas Bock <nicolasbock@gmail.com>
2023-08-20 08:03:14 -0600

    Refactor ProcessInputFiles
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- files_test.go
+++ files_test.go
@@ -5,12 +5,20 @@ import (
 	"testing"
 )
 
-func TestProcessInputFiles(t *testing.T) {
+func compareContentTypes(a, b ContentType, t *testing.T) {
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("got\n%s\nexpected\n%s", a, b)
+	}
+}
+
+func TestProcessInputFiles1(t *testing.T) {
 	var input []string = []string{
 		"Line one",
 		"Line two",
 		"Line three",
+		"Line four",
 	}
+	multiline = 1
 	var key KeyType = KeyType{
 		Type: NoKey,
 	}
@@ -18,9 +26,47 @@ func TestProcessInputFiles(t *testing.T) {
 		{Lines: []string{"Line one"}, Fields: []string{"Line", "one"}, CompareLine: "Line one"},
 		{Lines: []string{"Line two"}, Fields: []string{"Line", "two"}, CompareLine: "Line two"},
 		{Lines: []string{"Line three"}, Fields: []string{"Line", "three"}, CompareLine: "Line three"},
+		{Lines: []string{"Line four"}, Fields: []string{"Line", "four"}, CompareLine: "Line four"},
 	}
 	var got ContentType = ProcessInputFiles(input, key)
-	if !reflect.DeepEqual(got, expected) {
-		t.Errorf("got\n%s\nexpected\n%s", got, expected)
-	}
+	compareContentTypes(got, expected, t)
 }
+
+func TestProcessInputFiles2(t *testing.T) {
+	var input []string = []string{
+		"Line one",
+		"Line two",
+		"Line three",
+		"Line four",
+	}
+	multiline = 2
+	var key KeyType = KeyType{
+		Type: NoKey,
+	}
+	var expected ContentType = ContentType{
+		{Lines: []string{"Line one", "Line two"}, Fields: []string{"Line", "one", "Line", "two"}, CompareLine: "Line one Line two"},
+		{Lines: []string{"Line three", "Line four"}, Fields: []string{"Line", "three", "Line", "four"}, CompareLine: "Line three Line four"},
+	}
+	var got ContentType = ProcessInputFiles(input, key)
+	compareContentTypes(got, expected, t)
+}
+
+// func TestProcessInputFiles3(t *testing.T) {
+	// var input []string = []string{
+		// "Line 1 a",
+		// "Line 2 b",
+		// "Line 3 c",
+		// "Line 4 d",
+	// }
+	// multiline = 3
+	// var key KeyType = KeyType{
+		// Type: SingleField,
+		// Keys: []int{2},
+	// }
+	// var expected ContentType = ContentType{
+		// {Lines: []string{"Line one", "Line two"}, Fields: []string{"Line", "one", "Line", "two"}, CompareLine: "Line one Line two"},
+		// {Lines: []string{"Line three", "Line four"}, Fields: []string{"Line", "three", "Line", "four"}, CompareLine: "Line three Line four"},
+	// }
+	// var got ContentType = ProcessInputFiles(input, key)
+	// compareContentTypes(got, expected, t)
+// }
