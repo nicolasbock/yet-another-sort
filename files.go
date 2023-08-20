d35ba0f38cec092729e9580814db0242fc40c550
Nicolas Bock <nicolasbock@gmail.com>
2023-08-20 08:03:14 -0600

    Refactor ProcessInputFiles
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- files.go
+++ files.go
@@ -32,41 +32,47 @@ func LoadFile(filename string) []string {
 	return lines
 }
 
-// LoadInputFiles loads the input file(s) and returns a concatenated list of lines.
+// ProcessInputFiles analyzes the input and splits the lines into multilines. It
+// uses the key to construct the part of the multiline that will be used for
+// comparisons.
 func ProcessInputFiles(lines []string, key KeyType) ContentType {
+	log.Debug().Msgf("Processing %d lines, key: %s", len(lines), key)
 	var contents ContentType = ContentType{}
 	var lastContentLine *ContentLineType
-	var lineNumber int
-	for _, line := range lines {
-		if lineNumber%multiline == 0 {
-			contents = append(contents, ContentLineType{})
-			lastContentLine = &contents[len(contents)-1]
-		}
-		var fields []string = strings.Split(line, fieldSeparator)
-		lastContentLine.Lines = append(lastContentLine.Lines, line)
-		lastContentLine.Fields = append(lastContentLine.Fields, fields...)
-		lineNumber++
-		if lineNumber%multiline == 0 {
-			switch key.Type {
-			case NoKey:
-				lastContentLine.CompareLine = strings.Join(lastContentLine.Fields, fieldSeparator)
-			case SingleField:
-				if len(lastContentLine.Fields) < key.Keys[0] {
-					log.Fatal().Msgf("multiline %d (%s) does not have enough keys",
-						lineNumber, lastContentLine.Lines)
-
-				}
-				lastContentLine.CompareLine = lastContentLine.Fields[key.Keys[0]-1]
-			case Remainder:
-				if len(lastContentLine.Fields) < key.Keys[0] {
-					log.Fatal().Msgf("multiline %d (%s) does not have enough keys",
-						lineNumber, lastContentLine.Lines)
-
-				}
-				lastContentLine.CompareLine = strings.Join(lastContentLine.Fields[key.Keys[0]-1:], fieldSeparator)
-			case SubSet:
-
+	for i := 0; i < len(lines); i += multiline {
+		contents = append(contents, ContentLineType{})
+		lastContentLine = &contents[len(contents)-1]
+		for j := 0; j < multiline; j++ {
+			var linenumber int = i + j
+			if linenumber >= len(lines) {
+				break
 			}
+
+			var fields []string = strings.Split(lines[linenumber], fieldSeparator)
+			lastContentLine.Lines = append(lastContentLine.Lines, lines[linenumber])
+			lastContentLine.Fields = append(lastContentLine.Fields, fields...)
+		}
+
+		switch key.Type {
+		case NoKey:
+			lastContentLine.CompareLine = strings.Join(lastContentLine.Fields, fieldSeparator)
+		case SingleField:
+			if len(lastContentLine.Fields) < key.Keys[0] {
+				log.Fatal().Msgf("multiline %s does not have enough keys", lastContentLine.Lines)
+			}
+			lastContentLine.CompareLine = lastContentLine.Fields[key.Keys[0]-1]
+		case Remainder:
+			if len(lastContentLine.Fields) < key.Keys[0] {
+				log.Fatal().Msgf("multiline %s does not have enough keys", lastContentLine.Lines)
+			}
+			lastContentLine.CompareLine = strings.Join(
+				lastContentLine.Fields[key.Keys[0]-1:], fieldSeparator)
+		case SubSet:
+			if len(lastContentLine.Fields) < key.Keys[1] {
+				log.Fatal().Msgf("multiline %s does not have enough keys", lastContentLine.Lines)
+			}
+			lastContentLine.CompareLine = strings.Join(
+				lastContentLine.Fields[key.Keys[0]-1:key.Keys[1]], fieldSeparator)
 		}
 	}
 	return contents
