12f36aa77b1aa2886e8fc62070414567508886d3
Nicolas Bock <nicolasbock@gmail.com>
2023-08-23 14:13:41 -0600

    Replace Go string compare with libc
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- sort.go
+++ sort.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"strings"
+	/*
+		#include <stdlib.h>
+		#include <string.h>
+	*/
+	"C"
+	"unsafe"
 )
 
 // SortContents sorts the content lines and returns a sorted list.
 func SortContents(contents ContentType) (sortedContents ContentType) {
 	sortedContents = append(sortedContents, contents...)
 
-	// Combine multilines with field-separator.
-	// Sort multilined content
-	// Split multilined content into original multilines
-
 	for i := range sortedContents {
+		var a = C.CString(sortedContents[i].CompareLine)
+		defer C.free(unsafe.Pointer(a))
 		for j := range sortedContents {
-			if strings.Compare(sortedContents[i].CompareLine, sortedContents[j].CompareLine) < 0 {
+			var b = C.CString(sortedContents[j].CompareLine)
+			defer C.free(unsafe.Pointer(b))
+			if int(C.strcoll(a, b)) < 0 {
 				var temp ContentLineType = sortedContents[i]
 				sortedContents[i] = sortedContents[j]
 				sortedContents[j] = temp
